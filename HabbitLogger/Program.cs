b508601c280a71edf518bec33293362d804bea74
m1chael888 <sofishmain@gmail.com>
2025-12-25 00:33:42 +0200

    method setup, exception handling

--- HabbitLogger/Program.cs
+++ HabbitLogger/Program.cs
@@ -4,7 +4,7 @@ namespace HabbitLogger
 { 
     internal class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             try
             {
@@ -19,33 +19,36 @@ namespace HabbitLogger
 
             void Menu()
             {
+                Console.Clear();
+                int errorCount = 0;
                 bool done = false;
                 string? input;
 
                 Console.WriteLine(@"//// Trackington Menu \\\\");
+                Console.WriteLine("\n1 - View your habit progress");
+                Console.WriteLine("2 - Update your habit progress");
+                Console.WriteLine("3 - Remove a habit and its history");
+                Console.WriteLine("4 - Add a new habit to track");
+                Console.WriteLine("5 - Close trackington");
+                Console.WriteLine("\nEnter the number of your desired operation:");
+
                 while (!done)
                 {
-                    Console.WriteLine("\n1 - View your habit progress");
-                    Console.WriteLine("2 - Update your habit progress");
-                    Console.WriteLine("3 - Remove a habit and its history");
-                    Console.WriteLine("4 - Add a new habit to track");
-                    Console.WriteLine("5 - Close trackington");
-                    Console.Write("\nEnter the number of your desired operation: ");
                     input = Console.ReadLine();
 
                     switch (input)
                     {
                         /*case "1":
-
+                            ReadHabit();
                             break;
                         case "2":
-
+                            UpdateHabit();
                             break;
                         case "3":
-
+                            DeleteHabit();
                             break;
                         case "4":
-
+                            CreateHabit();
                             break;*/
                         case "5":
                             Console.WriteLine();
@@ -53,31 +56,53 @@ namespace HabbitLogger
                             Environment.Exit(0);
                             break;
                         default:
-                            Console.Clear();
+                            errorCount++;
+                            if (errorCount > 3) Menu();
+
                             Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("!!! Please enter a valid menu number (1-5)");
                             Console.ForegroundColor = ConsoleColor.White;
                             break;
                     }
                 }
+            }
 
-                void InitializeDb()
+            void ReadHabit()
+            {
+
+            }
+
+            void UpdateHabit()
+            {
+
+            }
+
+            void DeleteHabit()
+            {
+
+            }
+
+            void CreateHabit()
                 {
-                    string db = "Data Source=trackington.db";
-                    using var connection = new SqliteConnection(db);
-                    connection.Open();
 
-                    var command = connection.CreateCommand();
-                    command.CommandText =
-                        @"CREATE TABLE IF NOT EXISTS hydrate(
+            }
+
+            void InitializeDb()
+            {
+                string db = "Data Source=trackington.db";
+                using var connection = new SqliteConnection(db);
+                connection.Open();
+                var command = connection.CreateCommand();
+
+                command.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS hydrate(
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         Date TEXT,
                         Qty INTEGER
                         )";
-                    command.ExecuteNonQuery();
+                command.ExecuteNonQuery();
 
-                    connection.Close();
-                }
+                connection.Close();
             }
         }
     }
